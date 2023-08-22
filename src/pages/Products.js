a019f913192f7d923c64343403185a5628e7b3a5
Arron <beale86@msn.com>
2023-08-22 19:47:42 +0000

    Import Link, add link to products and home pages

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function ProductsPage() {
-    return <h1>Products Page</h1>;
+    return (
+        <>
+          <h1>Products Page</h1>
+          <p>Go to <Link to='/'>homepage</Link>.</p>
+        </>
+      );
 }
 
 export default ProductsPage;
