6d78923b2a59dc3746415a14047c0e7469c6e8e6
Arron <beale86@msn.com>
2023-08-22 19:36:50 +0000

    Add alternative way to create routes and router, comment out for demo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // Import createRoutesFromElements for alternative way
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+
+// Alternative way to create routes, older
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage />} />
+//     <Route path='/products' element={<ProductsPage />} />
+//   </Route>
+// )
+
+
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/products', element: <ProductsPage />}
 ]);
+
+// Alternative way to create router, older
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
