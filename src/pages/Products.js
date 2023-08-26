3e9b3cc857a990c42573391e0a8fac7476c0097d
Arron <beale86@msn.com>
2023-08-26 18:54:18 +0000

    Add ProductDetails.js, add product list to Products.js, add route to products in app.js

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,6 +4,11 @@ function ProductsPage() {
     return (
         <>
           <h1>Products Page</h1>
+          <ul>
+            <li>Product 1</li>
+            <li>Product 2</li>
+            <li>Product 3</li>
+          </ul>
           <p>Go to <Link to='/'>homepage</Link>.</p>
         </>
       );
