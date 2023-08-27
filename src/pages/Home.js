f54d320cda78e7a2c927c1574c69e44639d6cede
Arron <beale86@msn.com>
2023-08-27 19:53:33 +0000

    Add Products array, add dynamic link generation for products to Products.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom'; // useNavigate
-
 function HomePage() {
   // const navigate = useNavigate();
 
@@ -11,7 +9,6 @@ function HomePage() {
   return (
     <>
       <h1>Homepage</h1>
-      <p>Go to <Link to='/products'>the list of products</Link>.</p>
       {/* <p>
         <button onClick={navigateHandler}>navigate</button>
       </p> */}
